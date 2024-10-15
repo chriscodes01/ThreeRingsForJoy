ed4afc3db92dd5d32be033ec697d5adf7ee5f3f1
chriscodes01 <chriscodes.business@gmail.com>
2024-10-15 01:10:18 -0500

    small changes

--- scripts/items/cat.gd
+++ scripts/items/cat.gd
@@ -4,7 +4,6 @@ var fileName = get_script().get_path().get_file()
 var itemName = fileName.get_slice(".", 0)
 
 func _on_body_entered(_body):
-	var isComplete = false
 	var scene = get_tree().get_current_scene()
 	if (scene.name == "CompletionScreen"):
 		return
--- scripts/items/dog.gd
+++ scripts/items/dog.gd
@@ -4,6 +4,9 @@ var fileName = get_script().get_path().get_file()
 var itemName = fileName.get_slice(".", 0)
 
 func _on_body_entered(_body):
+	var scene = get_tree().get_current_scene()
+	if (scene.name == "CompletionScreen"):
+		return
 	if (_body.name == "Player"):
 		GAMEMANAGER.increment_mergeItem(itemName)
 		queue_free()
